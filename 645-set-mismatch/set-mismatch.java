6a0d4db34d8bb090f6dcdc97eb71d38fb33752ee
Udhaya-46 <selvamudhaya46@gmail.com>
2026-07-24 13:18:30 +0530

    Time: 1 ms (99.98%) | Memory: 47.2 MB (94.35%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1];
+
+        int duplicate = -1;
+        int missing = -1;
+
+        // Find the duplicate
+        for (int num : nums) {
+            if (seen[num]) {
+                duplicate = num;
+            } else {
+                seen[num] = true;
+            }
+        }
+
+        // Find the missing number
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                missing = i;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
